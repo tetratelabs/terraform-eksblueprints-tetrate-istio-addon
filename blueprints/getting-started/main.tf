dd85ded97159ca1db6c18734dcc56295aa7f1f5a
Petr McAllister <petr.mcallister@gmail.com>
2022-07-14 13:47:45 -0700

    lint items

--- blueprints/getting-started/main.tf
+++ blueprints/getting-started/main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 1.0.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.66.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.6.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+  }
+}
+
 provider "aws" {
   region = local.region
 }
